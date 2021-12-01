e24ad8880d3569b4e7232b540221c184617c57e5
Hans-Martin <hmduringhof@gmail.com>
2021-12-01 06:33:44 +0100

    feat Solution day 1

--- src/day01.cpp
+++ src/day01.cpp
@@ -2,9 +2,38 @@
 
 using namespace std;
 
+int slidingSum(vector<int>& v, int k) {
+    int sum = v[k]+v[k-1]+v[k-2];
+    return sum;
+}
+
 int main() {
 
-    cout << "Hello AoC2021!" << endl;
+    std::vector<int> depths = readInput("../input/input01.txt");
+
+    int nrOfIncreases = 0;
+
+    for (int i = 1; i < depths.size(); i++) {
+
+        int diff = depths[i] - depths[i - 1];
+        if (diff > 0) {
+            nrOfIncreases++;
+        }
+    }
+
+    cout << "Part 1: the number of times a depth measurement increases is " << nrOfIncreases << endl;
+
+    nrOfIncreases = 0;
+
+    for (int i = 3; i < depths.size(); i++) {
+
+        int diff = slidingSum(depths,i) - slidingSum(depths, i - 1);
+        if (diff > 0) {
+            nrOfIncreases++;
+        }
+    }
+
+    cout << "Part 2: the number of times the sum of measurements in a 3 sample sliding window increases is " << nrOfIncreases << endl;
     
     return 0;
 }
