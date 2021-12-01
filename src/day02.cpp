b8be689f9705472b9e55a9eead389755c4314158
Hans-Martin <hmduringhof@gmail.com>
2021-12-01 06:44:49 +0100

    feat Preps for day 2

---
+++ src/day02.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    std::vector<int> depths = readInput("../input/input01.txt");
+
+    cout << "Day 2 is not ready..." << endl;
+    
+    return 0;
+}
