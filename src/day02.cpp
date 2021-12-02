e314635abc8abc5b399a2d69a7b6fd94550036f6
Hans-Martin <hmduringhof@gmail.com>
2021-12-02 06:19:28 +0100

    feat Solution day 2

--- src/day02.cpp
+++ src/day02.cpp
@@ -2,11 +2,57 @@
 
 using namespace std;
 
+struct Instruction {
+    string command;
+    int value;
+};
+
+struct State {
+    int pos;
+    int depth;
+    int aim;
+};
+
+vector<Instruction> readInstructions(vector<string> lines) {
+    vector<Instruction> instructions;
+    for (auto line : lines) {
+        stringstream ss(line);
+        Instruction instruction;
+        ss >> instruction.command;
+        ss >> instruction.value;
+        instructions.push_back(instruction);
+    }
+    return instructions;
+}
+
 int main() {
 
-    std::vector<int> depths = readInput("../input/input01.txt");
+    vector<string> lines = readLines("../input/input02.txt");
+    vector<Instruction> instructions = readInstructions(lines);
 
-    cout << "Day 2 is not ready..." << endl;
+    int forward = 0;
+    int depth = 0;
+    int aim = 0;
+
+    State part1 = {0, 0, 0};
+    State part2 = {0, 0, 0};
+
+    for (Instruction instruction : instructions) {
+        if (instruction.command == "forward") {
+            part1.pos += instruction.value;
+            part2.pos += instruction.value;
+            part2.depth += instruction.value* part2.aim;
+        } else if (instruction.command == "down") {
+            part1.depth += instruction.value;
+            part2.aim += instruction.value;
+        } else if (instruction.command == "up") {
+            part1.depth -= instruction.value;
+            part2.aim -= instruction.value;
+        }
+    }
+
+    cout << "Part 1 - The submarine's depth multiplied by its position is: " << part1.depth * part1.pos << endl;
+    cout << "Part 2 - The submarine's depth multiplied by its position is: " << part2.depth * part2.pos << endl;
     
     return 0;
 }
