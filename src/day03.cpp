8ca6a2bdf1ca367490d150da4f006fcb64a9d64f
Hans-Martin <hmduringhof@gmail.com>
2021-12-02 15:14:50 +0100

    feat Preps for day 3

---
+++ src/day03.cpp
@@ -0,0 +1,13 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> lines = readLines("../input/input02.txt");
+    
+    cout << "Part 1 - Day 3 is not ready yet... " << endl;
+    cout << "Part 2 - Day 3 is not ready yet... " << endl;
+    
+    return 0;
+}
