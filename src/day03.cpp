a4353c3472771c2a7f647ec6879e3d6868bad328
Hans-Martin <hmduringhof@gmail.com>
2021-12-03 07:19:23 +0100

    refactor Rearrange string to vector with bool

--- src/day03.cpp
+++ src/day03.cpp
@@ -2,16 +2,28 @@
 
 using namespace std;
 
+vector<bool> string2bool(string s) {
+    vector<bool> res;
+    for (char c : s) {
+        res.push_back(c == '1');
+    }
+    return res;
+}
+
 int main() {
 
-    vector<string> codes = readLines("../input/input03.txt");
+    vector<string> lines = readLines("../input/input03.txt");
+    vector<vector<bool>> input;
+    for (string line : lines) {
+        input.push_back(string2bool(line));
+    }
 
-    vector<int> counts(codes[0].size(), 0);
+    vector<int> counts(input[0].size(), 0);
 
     
-    for (auto code : codes) {
-        for (int i = 0; i < codes[0].size(); i++) {
-            if (code[i] == '1') {
+    for (auto code : input) {
+        for (int i = 0; i < code.size(); i++) {
+            if (code[i]) {
                 counts[i]++;
             }
         }
@@ -20,10 +32,10 @@ int main() {
     int epsilon = 0;
     int gamma = 0;
     for (int j = 0; j < counts.size(); j++) {
-        if (counts[j] > codes.size()/2) {
-            epsilon += pow(2, counts.size()-j-1);
-        } else {
+        if (counts[j] > input.size()/2) {
             gamma += pow(2, counts.size()-j-1);
+        } else {
+            epsilon += pow(2, counts.size()-j-1);
         }
     }
     
