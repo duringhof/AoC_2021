34ac7797d00c51664afa1122cbd9a0481ca54a73
Hans-Martin <hmduringhof@gmail.com>
2021-12-03 21:14:11 +0100

    chore Preps for day 4

---
+++ src/day04.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> lines = readLines("../input/input03.txt");
+
+    cout << "Day 4 is not ready" << endl;
+    
+    return 0;
+}
