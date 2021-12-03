27f32fe7a83f3d9df1208c807d384533592b2b55
Hans-Martin <hmduringhof@gmail.com>
2021-12-03 06:43:16 +0100

    feat Solution day 3 part 1

--- src/day03.cpp
+++ src/day03.cpp
@@ -4,9 +4,30 @@ using namespace std;
 
 int main() {
 
-    vector<string> lines = readLines("../input/input02.txt");
+    vector<string> codes = readLines("../input/input03.txt");
+
+    vector<int> counts(codes[0].size(), 0);
+
     
-    cout << "Part 1 - Day 3 is not ready yet... " << endl;
+    for (auto code : codes) {
+        for (int i = 0; i < codes[0].size(); i++) {
+            if (code[i] == '1') {
+                counts[i]++;
+            }
+        }
+    }
+
+    int epsilon = 0;
+    int gamma = 0;
+    for (int j = 0; j < counts.size(); j++) {
+        if (counts[j] > codes.size()/2) {
+            epsilon += pow(2, counts.size()-j-1);
+        } else {
+            gamma += pow(2, counts.size()-j-1);
+        }
+    }
+    
+    cout << "Part 1 - The power of the submarine is " << gamma*epsilon << endl;
     cout << "Part 2 - Day 3 is not ready yet... " << endl;
     
     return 0;
