3635e94655303320292a513065d4db4263a2bd7d
Hans-Martin <hmduringhof@gmail.com>
2021-12-04 09:47:59 +0100

    chore Preps for day 5

---
+++ src/day05.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> lines = readLines("../input/input04.txt");
+
+    cout << "Part 1 - " << "Day 5 is not ready yet!!!" << endl;
+
+    return 0;
+}
