ec0126dabbe5b8fa7a6a51480fdc0ad856424a1b
Hans-Martin <hmduringhof@gmail.com>
2021-12-05 07:27:59 +0100

    chire Preps for day 6

---
+++ src/day06.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> input = readLines("../input/input05.txt");
+
+    cout << "PDay 6 is not ready yet !!!" << endl;
+
+    return 0;
+}
