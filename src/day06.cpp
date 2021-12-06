1aaaa7dc274f96b7357b4e6d4c50966f1ddd7ee0
Hans-Martin <hmduringhof@gmail.com>
2021-12-06 06:23:47 +0100

    feat Solution dasy 6 part 1

--- src/day06.cpp
+++ src/day06.cpp
@@ -2,11 +2,48 @@
 
 using namespace std;
 
+struct Fish {
+    int timer;
+};
+
+vector<int> readInputWithCommas(string file){
+    // Reads input file and return a vector<int> called input
+	vector<int> input;
+	ifstream inputFile(file);
+	int number;
+    char dummy;
+	while (inputFile >> number){
+		input.push_back(number);
+        inputFile >> dummy;
+	}
+	inputFile.close();
+	return input;
+};
+
 int main() {
 
-    vector<string> input = readLines("../input/input05.txt");
+    vector<int> input = readInputWithCommas("../input/input06.txt");
 
-    cout << "Day 6 is not ready yet !!!" << endl;
+    vector<Fish> fishes;
+    for (int i = 0; i < input.size(); i++) {
+        fishes.push_back({input[i]});
+    }
+
+    Fish newfish{9};
+
+    for (int day=1; day<81; day++) {
+        for (int i = 0; i < fishes.size(); i++) {
+            if (fishes[i].timer == 0) {            
+                fishes.push_back(newfish);
+                fishes[i].timer = 7;
+            }
+            fishes[i].timer--;
+        }
+    }
+
+    cout << "Part 1 - The number of lanternfish after 80 days is "
+         << fishes.size()
+         << endl;
 
     return 0;
 }
