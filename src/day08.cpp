bf6edd9ecebc894dc0b91fae60ddc09d3dc18b9c
Hans-Martin <hmduringhof@gmail.com>
2021-12-07 21:37:06 +0100

    chore Preps day 8

---
+++ src/day08.cpp
@@ -0,0 +1,18 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<int> input = readInputWithCommas("../input/test.txt");
+
+    cout << "Part 1 - Not ready yet "
+         // << 
+         << endl;
+
+    cout << "Part 2 - Not ready yet "
+         // << 
+         << endl;
+
+    return 0;
+}
