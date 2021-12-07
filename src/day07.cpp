9342caa62785b2eca9f1102058303eca53d9e2b3
Hans-Martin <hmduringhof@gmail.com>
2021-12-07 07:57:38 +0100

    feat Solution day 7 complete

--- src/day07.cpp
+++ src/day07.cpp
@@ -2,16 +2,49 @@
 
 using namespace std;
 
+int calcFuel(int start, int target, bool progressive) {
+    int fuel = 0;
+    if (abs(target - start) > 0) {
+        for (int i = 0; i < abs(target - start); i++) {
+            fuel += 1 + progressive*i;
+        }
+    }
+    return fuel;
+}
+
+int highestValue(vector<int>& v) {
+    int max = 0;
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] > max) {
+            max = v[i];
+        }
+    }
+    return max;
+}
+
+int optimalFuel(vector<int>& v, bool progressive) {
+    int min_fuel = INT_MAX;
+    int fuel;
+    for (int i = 0; i < highestValue(v); i++) {
+        fuel = 0;
+        for (int j = 0; j < v.size(); j++) {
+            fuel += calcFuel(v[j], i, progressive);
+        }
+        min_fuel = (fuel < min_fuel ? fuel : min_fuel);
+    }
+    return min_fuel;
+}
+
 int main() {
 
-    vector<int> input = readInputWithCommas("../input/test.txt");
+    vector<int> input = readInputWithCommas("../input/input07.txt");
 
-    cout << "Part 1 - Not ready yet "
-         //<< res_part1
+    cout << "Part 1 - The total fuel cost for optimally aligning all crab submarines is "
+         << optimalFuel(input, false)
          << endl;
 
-    cout << "Part 2 - Not ready yet "
-         //<< res_part2
+    cout << "Part 2 - The corrected fuel cost for optimally aligning all crab submarines is "
+         << optimalFuel(input, true)
          << endl;
 
     return 0;
