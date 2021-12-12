32c456914026a5421764b51c9a9ee841caca7946
Hans-Martin <hmduringhof@gmail.com>
2021-12-12 11:48:58 +0100

    chore Preps for day 9

---
+++ src/day09.cpp
@@ -0,0 +1,19 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+     vector<string> lines = readLines("../input/input09.txt");
+
+     cout << "Part 1 - Not ready yet... "
+          //<< counter
+          //<< " times."
+          << endl;
+
+     cout << "Part 2 - Not ready yet... "
+          //<< answer 
+          << endl;
+
+    return 0;
+}
