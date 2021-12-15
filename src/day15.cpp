a61e24469040bae072475b933755deddaa3c3bcf
Hans-Martin <hmduringhof@gmail.com>
2021-12-15 09:06:59 +0100

    chore Preps for day 15

---
+++ src/day15.cpp
@@ -0,0 +1,16 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> lines = readLines("../input/test.txt");
+
+    cout << "Part 1 - Not done..."
+         << endl;
+
+    cout << "Part 2 - Not done..."
+         << endl;
+
+    return 0;
+}
